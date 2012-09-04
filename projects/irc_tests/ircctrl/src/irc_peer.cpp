56d751d5c9a45ae5c65d7f4b74778a6781dff280
z80 <litedictteam@gmail.com>
2012-09-04 15:24:24 +0400

    In progress.

--- projects/irc_tests/ircctrl/src/irc_peer.cpp
+++ projects/irc_tests/ircctrl/src/irc_peer.cpp
@@ -2,6 +2,12 @@
 #include "irc_peer.h"
 #include "libircclient.h"
 
+#include <stdio.h>
+#include <errno.h>
+#include <string.h>
+
+#include <boost/thread.hpp>
+
 //* Static functions declaration.
 static void event_connect( irc_session_t * session,
 		                      const char * event,
@@ -13,6 +19,16 @@ static void event_nick( irc_session_t * session,
 		                   const char * origin,
 		                   const char ** params,
 		                   unsigned int count );
+static void event_channel( irc_session_t * session,
+		                      const char * event,
+		                      const char * origin,
+		                      const char ** params,
+		                      unsigned int count );
+static void event_numeric( irc_session_t * session,
+		                      unsigned int event,
+		                      const char * origin,
+		                      const char ** params,
+		                      unsigned int count );
 
 //* Private data class for storaging data which is not convenient to #include
 //* in destination application.
@@ -22,7 +38,9 @@ public:
 	PD() {}
 	~PD() {}
 	std::string host;
-	it
+	int port;
+	std::string nick, realName;
+	std::string userName, password;
 };
 
 IrcPeer::IrcPeer()
@@ -35,6 +53,24 @@ IrcPeer::~IrcPeer()
 	delete pd;
 }
 
+void IrcPeer::setHost( const std::string & host, int port )
+{
+	pd->host = host;
+	pd->port = port;
+}
+
+void IrcPeer::setLogin( const std::string & userName, const std::string & password )
+{
+	pd->userName = userName;
+	pd->password = password;
+}
+
+void IrcPeer::setNick( const std::string & nick, const std::string & realName )
+{
+	pd->nick     = nick;
+	pd->realName = realName;
+}
+
 void IrcPeer::connect( const std::string address, int port )
 {
 	irc_callbacks_t	callbacks;
@@ -45,11 +81,13 @@ void IrcPeer::connect( const std::string address, int port )
 	// Set up the callbacks we will use
 	callbacks.event_connect = event_connect;
 	callbacks.event_channel = event_channel;
+	callbacks.event_nick    = event_nick;
+	callbacks.event_numeric = event_numeric;
 
 	// And create the IRC session; 0 means error
 	irc_session_t * s = irc_create_session( &callbacks );
 	irc_set_ctx( s, &pd );
-
+    /*
 	if ( irc_connect( s, host.data(), port, 0, nick, 0, 0 ) )
 	{
 		printf( "Could not connect: %s\n", irc_strerror(irc_errno(s)) );
@@ -61,7 +99,7 @@ void IrcPeer::connect( const std::string address, int port )
 	{
 		printf ( "Could not connect or I/O error: %s\n", irc_strerror( irc_errno(s) ) );
 		return 2;
-	}
+	}*/
 
 }
 
--- projects/irc_tests/ircctrl/src/irc_peer.h
+++ projects/irc_tests/ircctrl/src/irc_peer.h
@@ -12,8 +12,11 @@ class IrcPeer
 public:
 	IrcPeer();
 	~IrcPeer();
+	void setHost( const std::string & host, int port = 6667 );
+	void setLogin( const std::string & userName, const std::string & password );
+	void setNick( const std::string & nick, const std::string & realName );
+	void connect( const std::string address = std::string(), int port = 6667 );
 protected:
-	void connect( const std::string address, int port = 6667 );
 	void send( const std::string & stri );
 private:
     class PD;
