d1b11daf55b61c848df5c0a9db68d6b0408878b6
z80 <litedictteam@gmail.com>
2012-09-17 00:53:46 +0400

    Filling qxmpp test.

--- projects/irc_tests/tests/qxmpp_test/src/peer_qxmpp.h
+++ projects/irc_tests/tests/qxmpp_test/src/peer_qxmpp.h
@@ -2,12 +2,33 @@
 #ifndef __QXMPP_PEER_H_
 #define __QXMPP_PEER_H_
 
-class QxmppPeer
+#include "QXmppClient.h"
+#include <boost/function.hpp>
+#include <boost/bind.hpp>
+#include <boost/bind/placeholders.hpp>
+#include <boost/thread.hpp>
+#include <boost/thread/mutex.hpp>
+#include <boost/thread/condition.hpp>
+
+class QxmppPeer: QXmppClient
 {
     Q_OBJECT
 public:
     QxmppPeer( QObject * parent = 0 );
     ~QxmppPeer();
+
+    bool connect( const std::string & jid, const std::string & password );
+    bool isConnected() const;
+    void send( const std::string & jid, const std::string & stri );
+
+public slots:
+    void connected();
+    void disconnected();
+    void error( QXmppClient::Error );
+    void messageReceived( const QXmppMessage & );
+
+private:
+
 };
 
 
