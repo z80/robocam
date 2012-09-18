009ad1fc36a203e4ae6979902d0204ede1dde140
z80 <bashkirov.sergey@gmail.com>
2012-09-18 14:50:16 +0400

    Started PeerQxmpp.

---
+++ projects/irc_tests/tests/qxmpp_test/src/peer_qxmpp.cpp
@@ -0,0 +1,5 @@
+
+#include "peer_qxmpp.h"
+
+
+
---
+++ projects/irc_tests/tests/qxmpp_test/src/peer_qxmpp.h
@@ -0,0 +1,27 @@
+
+#ifndef __PEER_QXMPP_H_
+#define __PEER_QXMPP_H_
+
+#include "peer_abst.h"
+
+class PeerQxmpp: public PeerAbst
+{
+public:
+    PeerQxmpp( PeerAbst::TInit );
+    ~PeerQxmpp();
+
+	bool isConnected();
+	bool send( const std::string & cmd );
+private:
+    class PD;
+    PD * pd;
+};
+
+
+
+
+#endif
+
+
+
+
