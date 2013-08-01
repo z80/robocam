4eb79895029d3792c5425fc1cdf86b524d975ad6
z80 <bashkirov.sergey@gmail.com>
2013-08-01 13:24:37 +0400

    Moto output on only when power to moto is on.

--- projects/ctrl-board/firmware/firmware/moto_ctrl.c
+++ projects/ctrl-board/firmware/firmware/moto_ctrl.c
@@ -32,25 +32,28 @@ void motoSetEn( uint16_t en )
 
 void motoSet( uint16_t en )
 {
-    if (en & 0x01 )
+    // When motor output is off power to the motor control chip is also off.
+    // To prevent powering through MCU pins turn pins on only if power is on.
+    uint16_t pwr = palReadPad( MOTO_EN_PORT, MOTO_EN_PIN );
+    if ( ( en & 0x01 ) && ( pwr ) )
 	    palSetPad( MOTO_1_PORT, MOTO_1_PIN );
 	else
 	    palClearPad( MOTO_1_PORT, MOTO_1_PIN );
     palSetPadMode( MOTO_1_PORT, MOTO_1_PIN, PAL_MODE_OUTPUT_PUSHPULL );
 
-    if ( en & 0x02 )
+    if ( ( en & 0x02 ) && ( pwr ) )
 	    palSetPad( MOTO_2_PORT, MOTO_2_PIN );
 	else
 	    palClearPad( MOTO_2_PORT, MOTO_2_PIN );
     palSetPadMode( MOTO_2_PORT, MOTO_2_PIN, PAL_MODE_OUTPUT_PUSHPULL );
 
-    if ( en & 0x04 )
+    if ( ( en & 0x04 ) && ( pwr ) )
 	    palSetPad( MOTO_3_PORT, MOTO_3_PIN );
 	else
 	    palClearPad( MOTO_3_PORT, MOTO_3_PIN );
     palSetPadMode( MOTO_3_PORT, MOTO_3_PIN, PAL_MODE_OUTPUT_PUSHPULL );
 
-    if ( en & 0x08 )
+    if ( ( en & 0x08 ) && ( pwr ) )
 	    palSetPad( MOTO_4_PORT, MOTO_4_PIN );
 	else
 	    palClearPad( MOTO_4_PORT, MOTO_4_PIN );
