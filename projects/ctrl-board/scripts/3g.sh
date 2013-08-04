a88ede04baf96c0b1ee73929a8d0ddea2c38dec8
z80 <bashkirov.sergey@gmail.com>
2013-08-04 23:11:26 +0400

    Running script is changed a bit.

--- projects/ctrl-board/scripts/3g.sh
+++ projects/ctrl-board/scripts/3g.sh
@@ -1,10 +1,9 @@
 #!/bin/sh
-#sleep 30
-touch /home/pi/projects/aaa.aaa
+
 echo "Trying to connect"
-#sudo /home/pi/projects/sakis3gz start APN="internet.mts.ru" OTHER="USBMODEM" USBINTERFACE="0" USBMODEM="12d1:155b"
-#sudo /home/pi/projects/sakis3gz start APN="internet.mts.ru" USBMODEM="12d1:155b"
+HOME=/home/pi/projects/software/build.app
+cd $HOME
 sudo ./sakis3gz connect OTHER="USBMODEM" USBMODEM="12d1:1506" USBINTERFACE="0" APN="internet.mts.ru"
 echo "Running regular software after connection"
-echo "Done"
+./host
 
