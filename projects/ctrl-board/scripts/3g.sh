f56aeceaee3e55b61be6c01c7150826e54d12d6c
z80 <bashkirov.sergey@gmail.com>
2013-08-04 22:12:57 +0400

    Switched to another modem.

--- projects/ctrl-board/scripts/3g.sh
+++ projects/ctrl-board/scripts/3g.sh
@@ -4,7 +4,7 @@ touch /home/pi/projects/aaa.aaa
 echo "Trying to connect"
 #sudo /home/pi/projects/sakis3gz start APN="internet.mts.ru" OTHER="USBMODEM" USBINTERFACE="0" USBMODEM="12d1:155b"
 #sudo /home/pi/projects/sakis3gz start APN="internet.mts.ru" USBMODEM="12d1:155b"
-sudo ./sakis3gz connect OTHER="USBMODEM" USBMODEM="12d1:1446" USBINTERFACE="0" APN="internet.mts.ru"
+sudo ./sakis3gz connect OTHER="USBMODEM" USBMODEM="12d1:1506" USBINTERFACE="0" APN="internet.mts.ru"
 echo "Running regular software after connection"
 echo "Done"
 
