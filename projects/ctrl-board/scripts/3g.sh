b46c5362e088d761a1c9da38f249e783c9a7fca4
z80 <bashkirov.sergey@gmail.com>
2013-08-07 20:12:12 +0400

    Added "&" to "host" run line.

--- projects/ctrl-board/scripts/3g.sh
+++ projects/ctrl-board/scripts/3g.sh
@@ -5,5 +5,5 @@ HOME=/home/pi/projects/software/build.app
 cd $HOME
 sudo ./sakis3gz connect OTHER="USBMODEM" USBMODEM="12d1:1506" USBINTERFACE="0" APN="internet.mts.ru"
 echo "Running regular software after connection"
-./host
+./host &
 
