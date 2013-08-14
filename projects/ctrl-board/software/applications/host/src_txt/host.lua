81adac7b2a4acf7235d9731146a2b0ef40d173eb
z80 <bashkirov.sergey@gmail.com>
2013-08-14 18:24:00 +0400

    Adjusted Lua script for host side.

--- projects/ctrl-board/software/applications/host/src_txt/host.lua
+++ projects/ctrl-board/software/applications/host/src_txt/host.lua
@@ -1,9 +1,6 @@
 
 require( "luactrlboard" )
 
-local POWER_ON_FIRST   = 60 * 60 * 3
-local POWER_ON_REGULAR = 60 * 60 * 3 
-local POWER_OFF        = 20
 local I2C_ADDR         = 1
 local status_table     = {}
 local TRIES_CNT        = 5
@@ -12,17 +9,25 @@ local TRY_DELAY        = 100
 local CMD_SET_POWER_TIMES = 1
 local CMD_POWER_RESET     = 2
 local CMD_SHUTDOWN_RESET  = 3
-local CMD_TEMP            = 4
-local CMD_CURR            = 5
-local CMD_SET_SOLAR_VOLT  = 6
-local CMD_SET_CHARGE_VOLT = 7
-local CMD_SET_MOTO_EN     = 8
-local CMD_SET_MOTO        = 9
-local CMD_SET_LIGHT       = 10
-local CMD_SET_LED         = 11
-local CMD_SETUP_OSC       = 15 -- 2 bytes period in seconds, 1 byte what to measure: current, temperature.
-local CMD_OSC_STATUS      = 16 -- 2 bytes number of measures in memory and 1 byte signals measured.
-local CMD_OSC             = 17 -- Get value/values by index. 0 - most recent, 1 - previous, 2 - prev-previous.
+local CMD_SET_SECONDS_PER_DAY = 4
+local CMD_SET_CURRENT_TIME    = 5
+local CMD_CURRENT_TIME        = 6
+local CMD_SET_WAKEUPS_CNT     = 7
+local CMD_SET_WAKEUP          = 8
+local CMD_TEMP            = 9
+local CMD_CURR            = 10
+local CMD_SET_SOLAR_VOLT  = 11
+local CMD_SET_CHARGE_VOLT = 12
+local CMD_SET_MOTO_EN     = 13
+local CMD_SET_MOTO        = 14
+local CMD_SET_LIGHT       = 15
+local CMD_SET_LED         = 16
+local CMD_BUCK_FB         = 17
+local CMD_BOOST_FB        = 18
+local CMD_SOLAR_FB        = 19
+local CMD_SETUP_OSC       = 20 -- 2 bytes period in seconds, 1 byte what to measure: current, temperature.
+local CMD_OSC_STATUS      = 21 -- 2 bytes number of measures in memory and 1 byte signals measured.
+local CMD_OSC             = 22 -- Get value/values by index. 0 - most recent, 1 - previous, 2 - prev-previous.
 
 
 function sendStatus()
@@ -183,26 +188,132 @@ function timeoutReset()
     return false
 end
 
-function powerSetup( onTime, offTime )
+function setPowerTimeout( onTime )
     local res = mcu:open()
     if ( res ) then
         local t = {}
-        t[1] = CMD_SET_POWER_TIMES
+        t[1] = CMD_SET_POWER_TIMEOUT
         t[2] = bit.band( onTime, 255 )
         t[3] = bit.rshift( onTime, 8 )
-        t[4] = bit.band( offTime, 255 )
-        t[5] = bit.rshift( offTime, 8 )
         res = mcu:write( I2C_ADDR, t )
         if ( not res ) then
             return false
         end
         status_table.onTime  = onTime
-	status_table.offTime = offTime
         mcu:close()
         return true
     end
     return false
 end
+
+function setSecondsPerDay( cnt )
+    local res = mcu:open()
+    if ( res ) then
+        local t = {}
+        t[1] = CMD_SET_SECONDS_PER_DAY
+        t[2] = bit.band( onTime, 255 )
+        t[3] = bit.band( bit.rshift( cnt, 8 ), 255 )
+	t[4] = bit.band( bit.rshift( cnt, 16 ), 255 )
+	t[5] = bit.band( bit.rshift( cnt, 24 ), 255 )
+        res = mcu:write( I2C_ADDR, t )
+        if ( not res ) then
+            return false
+        end
+        status_table.onTime  = onTime
+        mcu:close()
+        return true
+    end
+    return false
+end
+
+function setCurrentTime( cnt )
+    local res = mcu:open()
+    if ( res ) then
+        local t = {}
+        t[1] = CMD_SET_CURRENT_TIME
+        t[2] = bit.band( cnt, 255 )
+        t[3] = bit.band( bit.rshift( cnt, 8 ), 255 )
+	t[4] = bit.band( bit.rshift( cnt, 16 ), 255 )
+	t[5] = bit.band( bit.rshift( cnt, 24 ), 255 )
+        res = mcu:write( I2C_ADDR, t )
+        if ( not res ) then
+            return false
+        end
+        status_table.curTime  = onTime
+        mcu:close()
+        return true
+    end
+    return false
+end
+
+function currentTime()
+    local res = mcu:open()
+    if ( res ) then
+        local t = {}
+        t[1] = CMD_CURRENT_TIME
+        t[2] = 0
+        t[3] = 0
+        res = mcu:write( I2C_ADDR, t )
+        if ( not res ) then
+            return false
+        end
+        sleep( TRY_DELAY )
+        res = mcu:read( I2C_ADDR, 3 )
+        if ( not res ) then
+            return false
+        end
+	if ( res[1] ~= CMD_CURRENT_TIME ) then
+            status_table.time = "not ready"
+	else
+            local v = res[2] + res[3] * 256 + res[4] * 256 * 256 + res[5] * 256 * 256 * 256
+            status_table.time = v
+	end
+        mcu:close()
+        return true
+    end
+    return false
+end
+
+
+
+function setWakeupsCnt( cnt )
+    local res = mcu:open()
+    if ( res ) then
+        local t = {}
+        t[1] = CMD_SET_WAKEUPS_CNT
+	t[2] = bit.band( cnt, 255 )
+        t[3] = 0
+	t[4] = 0
+        res = mcu:write( I2C_ADDR, t )
+        if ( not res ) then
+            return false
+        end
+        mcu:close()
+        return true
+    end
+    return false
+end
+
+function setWakeup( ind, t )
+    local res = mcu:open()
+    if ( res ) then
+        local t = {}
+        t[1] = CMD_SET_CURRENT_TIME
+	t[2] = ind
+        t[3] = bit.band( t, 255 )
+        t[4] = bit.band( bit.rshift( t, 8 ), 255 )
+	t[5] = bit.band( bit.rshift( t, 16 ), 255 )
+	t[6] = bit.band( bit.rshift( t, 24 ), 255 )
+        res = mcu:write( I2C_ADDR, t )
+        if ( not res ) then
+            return false
+        end
+        mcu:close()
+        return true
+    end
+    return false
+end
+
 
 function temp()
     local res = mcu:open()
@@ -220,9 +331,13 @@ function temp()
         if ( not res ) then
             return false
         end
-        local v = res[2] + res[3] * 256
-	local t = ( v * (3300 / 4095) - 500 ) / 10
-        status_table.temp = t
+	if ( res[1] ~= CMD_TEMP ) then
+            status_table.temp = "not ready"
+	else
+            local v = res[2] + res[3] * 256
+            local t = ( v * (3300 / 4095) - 500 ) / 10
+            status_table.temp = t
+	end
         mcu:close()
         return true
     end
@@ -245,11 +360,15 @@ function curr()
         if ( not res ) then
             return false
         end
-	local i = res[2] + res[3] * 256 
-	-- V = I(A) * 0.3(Ohm) * 11(gain)
-	-- A(adc) = 4095 / 3.3 * I + 2047(shift to range center with another OpAmp)
-	i = 0.2442 * (i - 2047) -- mA
-        status_table.curr = i
+	if ( res[1] ~= CMD_CURR ) then
+            status_table.curr = "not ready"
+	else
+            local i = res[2] + res[3] * 256 
+            -- V = I(A) * 0.3(Ohm) * 11(gain)
+            -- A(adc) = 4095 / 3.3 * I + 2047(shift to range center with another OpAmp)
+            i = 0.2442 * (i - 2047) -- mA
+            status_table.curr = i
+	end
         mcu:close()
         return true
     end
