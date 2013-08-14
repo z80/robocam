9d1a4a54a9eefef0af326e05d32c3b5ec5d13cf4
z80 <bashkirov.sergey@gmail.com>
2013-08-14 18:23:45 +0400

    Corrected command indices.

--- projects/ctrl-board/firmware/firmware/i2c_slave_conf.h
+++ projects/ctrl-board/firmware/firmware/i2c_slave_conf.h
@@ -15,23 +15,24 @@
 #define CMD_SHUTDOWN_RESET      3
 #define CMD_SET_SECONDS_PER_DAY 4
 #define CMD_SET_CURRENT_TIME    5
-#define CMD_SET_WAKEUPS_CNT     6
-#define CMD_SET_WAKEUP          7
-#define CMD_TEMP                8
-#define CMD_CURR                9
-#define CMD_SET_SOLAR_VOLT      10
-#define CMD_SET_CHARGE_VOLT     11
-#define CMD_SET_MOTO_EN         12
-#define CMD_SET_MOTO            13
-#define CMD_SET_LIGHT           14
-#define CMD_SET_LED             15
-#define CMD_BUCK_FB             16
-#define CMD_BOOST_FB            17
-#define CMD_SOLAR_FB            18
+#define CMD_CURRENT_TIME        6
+#define CMD_SET_WAKEUPS_CNT     7
+#define CMD_SET_WAKEUP          8
+#define CMD_TEMP                9
+#define CMD_CURR                10
+#define CMD_SET_SOLAR_VOLT      11
+#define CMD_SET_CHARGE_VOLT     12
+#define CMD_SET_MOTO_EN         13
+#define CMD_SET_MOTO            14
+#define CMD_SET_LIGHT           15
+#define CMD_SET_LED             16
+#define CMD_BUCK_FB             17
+#define CMD_BOOST_FB            18
+#define CMD_SOLAR_FB            19
 // Commands for power oscilloscope.
-#define CMD_SETUP_OSC           19 // 2 bytes period in seconds, 1 byte what to measure: current, temperature.
-#define CMD_OSC_STATUS          20 // 2 bytes number of measures in memory and 1 byte signals measured.
-#define CMD_OSC                 21 // Get value/values by index. 0 - most recent, 1 - previous, 2 - prev-previous.
+#define CMD_SETUP_OSC           20 // 2 bytes period in seconds, 1 byte what to measure: current, temperature.
+#define CMD_OSC_STATUS          21 // 2 bytes number of measures in memory and 1 byte signals measured.
+#define CMD_OSC                 22 // Get value/values by index. 0 - most recent, 1 - previous, 2 - prev-previous.
 // To be continued.
 // .....
 #define CMD_PAWN_SET_IO         100
